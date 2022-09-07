03bea6d6d9b8557b687ff5eef02abd25bc229e7e
Moley456 <kingstarwei10@gmail.com>
2022-09-07 21:15:37 +0800

    update task A1 files

--- app/index.js
+++ app/index.js
@@ -7,17 +7,17 @@ app.use(express.static('public'));
 app.engine('html', ejs.renderFile);
 app.set('view engine', 'html');
 
-const PORT = 8080;
+const PORT = 8000;
 const HOST = '0.0.0.0';
 
 app.get('/', (req, res) => {
-  res.render(path.join(__dirname + '/index.html'));
+    res.render(path.join(__dirname + '/index.html'));
 });
 
 app.get('/success', (req, res) => {
-  res.send('Hello World Success!');
-})
+    res.send('Hello World Success!');
+});
 
 app.listen(PORT, HOST, () => {
-  console.log(`Example app listening on port ${PORT}`);
+    console.log(`Example app listening on port ${PORT}`);
 });
